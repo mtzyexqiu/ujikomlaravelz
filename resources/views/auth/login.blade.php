bc0bdbae8ea35f22e65a4ae1365786ef65d8e3f7
Matius <gameeza2712@gmail.com>
2025-02-07 11:02:13 +0700

    matius|070225| delete fitur remember

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -96,18 +96,12 @@
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
-                <!-- Remember Me -->
-                <div class="block mt-4">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                    </label>
-                </div>
+
 
                 <!-- Forgot Password -->
                 <div class="text-right mb-6">
                     @if (Route::has('password.request'))
-                        <a href="{{ route('password.request') }}" class="text-sm text-blue-500 hover:underline">Forgot password?</a>
+                        <a href="{{ route('password.request') }}" class="text-sm text-blue-500 hover:underline"></a>
                     @endif
                 </div>
 
